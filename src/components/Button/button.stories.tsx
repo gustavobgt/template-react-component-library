44ec82af24ce6917cda59c6ca596d5e5305205f3
gustavobgt <gustavo_bgt97@hotmail.com>
2022-08-30 08:00:45 -0300

    feat: configure storybook and create a better customizable button component

---
+++ src/components/Button/button.stories.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Story, Meta} from '@storybook/react';
+
+import { Button, ButtonProps } from '.';
+
+const meta: Meta = {
+  title: 'Example/Button',
+  component: Button,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+};
+
+export default meta;
+
+const Template: Story<ButtonProps> = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+
+Primary.args = {
+  primary: true,
+  label: 'Button',
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  label: 'Button',
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  size: 'large',
+  label: 'Button',
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  size: 'small',
+  label: 'Button',
+};
--- src/components/Button/index.ts
+++ src/components/Button/index.ts
@@ -1,1 +1,2 @@
-export {default as Button} from './Button'
+export { Button } from "./button.component";
+export * from "./button.types";
